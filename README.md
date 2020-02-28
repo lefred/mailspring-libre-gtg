de62b288676df37dae43d13a896ef0be5041ecb0
Frederic Descamps <lefred.descamps@gmail.com>
2020-02-28 16:06:40 +0100

    initial commit (this plugin is not yet working)
    Signed-off-by: Frederic Descamps <lefred.descamps@gmail.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+## Mailspring Libre GTG Plugin
+
+This plugin add a new task to GTG (Get Things Gnome) from a mail.
+
+
---
+++ src/my-gtg.jsx
@@ -0,0 +1,52 @@
+import { React, FocusedContactsStore } from "mailspring-exports";
+import { RetinaImg } from 'mailspring-component-kit';
+
+
+export default class MyGtg extends React.Component {
+  static displayName = "MyGtg";
+
+  _toGtg() {
+      // Function calling GTG via dbus
+      var DBus = require("dbus");
+      //var bus = DBus.getBus('session');
+      //bus.getInterface('org.gnome.GTG', '/org/gnome/GTG', 'org.gnome.GTG', function(err, iface) {
+      //    if (err) {
+      //        return console.log(err)
+      //    }
+	  //    iface.OpenNewTask("Test from NodeJS","This is the body", { timeout: 10 }, function(err, result) {
+      //        return console.log("Task added in GTG");
+      //    });
+      //});
+      return console.log("Task added in GTG");
+  }
+
+  _renderContent() {
+    // Create the button's image on with the right style 
+    return(
+    <RetinaImg
+        url="mailspring://gtg/assets/task@2x.png"
+        mode={RetinaImg.Mode.ContentIsMask}
+        />
+    );
+  }
+
+  render() {
+    // Create the button itself
+    const content = this._renderContent();
+    return <button
+                className="btn btn-toolbar"
+                title="Create a new GTG task"
+                onClick={this._toGtg} >
+                    {content}
+          </button>
+  }
+}
+
+// Providing container styles tells the app how to constrain
+// the column your component is being rendered in. The min and
+// max size of the column are chosen automatically based on
+// these values.
+MyGtg.containerStyles = {
+  order: 0,
+  flexShrink: 0
+};
